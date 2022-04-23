2504439e8c94fe5771a4c3a1dba66f8a3031b07c
ShreyangiPrasad25 <prasad.shrey@northeastern.edu>
2022-04-23 16:04:58 -0400

    add registration functionality

--- RaiseYourVoice_Ecosystem 3/src/Service/StaffRegistration.java
+++ RaiseYourVoice_Ecosystem 3/src/Service/StaffRegistration.java
@@ -25,7 +25,19 @@ public class StaffRegistration {
 	Connection con= DBcon.getDbcon();
 	try{
 	
-	
+	PreparedStatement stmt=con.prepareStatement("insert into STAFF values(?,?,?,?,?,?,?,?,?,?)");
+    
+	                stmt.setString(1,staff.getName());
+                        stmt.setLong(2, staff.getContact());
+                        stmt.setString(3,staff.getEmail());
+                        stmt.setString(4, staff.getPassword());
+                        stmt.setString(5,staff.getCity());
+                        stmt.setString(6,staff.getEnterprise());
+                        stmt.setString(7,staff.getDepartment());
+                        stmt.setString(8,staff.getRole());
+                        stmt.setString(9,staff.getSecurity_question());
+                        stmt.setString(10,staff.getWorking_Status());
+                        stmt.executeQuery();
 	JOptionPane.showMessageDialog( null, staff.getName()+" Registered Sucessfully\"");
         System.out.println("-------------Registeration Successful------------------");
         stmt.close();

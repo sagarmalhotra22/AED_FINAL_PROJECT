a9ddc066b166120e59dead5a6e83e80b10ae259c
unknown <malhotra.sag@northeastern.edu>
2022-04-23 09:43:30 -0400

    Created project and added homepage

---
+++ RaiseYourVoice_Ecosystem 3/src/Interface/HomePage.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package Interface;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+/**
+ *
+ *  
+ */
+ class HomePage extends javax.swing.JFrame {
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnLogin;
+    private javax.swing.JButton btnRegister;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel lblBackPic;
+    private javax.swing.JPanel pnlMainHome;
+    // End of variables declaration//GEN-END:variables
+}

e82bce3b664c70817da2fb15dc874ae008e3f9a1
ShreyangiPrasad25 <prasad.shrey@northeastern.edu>
2022-04-23 17:11:42 -0400

    Created staff class

---
+++ RaiseYourVoice_Ecosystem 3/src/Service/Staff.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Service;
+
+/**
+ *
+ *  
+ */
+public class Staff {
+    String name;
+    long contact;
+    String email;
+    String password;
+    String city;
+    String enterprise;
+    String department;
+    String role;
+    String security_question;  
+    String Working_Status;
+
+  
+    
+    
+}

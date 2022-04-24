8549ef8ecb256ab944522678b3f43f8081624476
ShreyangiPrasad25 <prasad.shrey@northeastern.edu>
2022-04-24 11:49:49 -0400

    Added victim registration

---
+++ RaiseYourVoice_Ecosystem 3/src/Service/VictimRegistration.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Service;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ *  
+ */
+public class VictimRegistration {
+    Victim victim;
+
+    public VictimRegistration(Victim victim) {
+        this.victim = victim;
+        
+      
+    
+}

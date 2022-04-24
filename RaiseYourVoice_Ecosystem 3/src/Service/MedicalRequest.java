f9fb8064110b05449fe709a6ea41e6b3bfa8c6b8
ShreyangiPrasad25 <prasad.shrey@northeastern.edu>
2022-04-24 12:26:49 -0400

    added medical request class and fields

---
+++ RaiseYourVoice_Ecosystem 3/src/Service/MedicalRequest.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Service;
+
+/**
+ *
+ *  
+ */
+public class MedicalRequest {
+    String Victim_email;
+    String Problem_Description; 
+    String Service;
+    String Gender; 
+    String Department;
+    String Assigned_To;
+    String Status;
+    String Request_Id;
+    String Notes;
+    String Request_Date;
+    String Update_Date;
+
+    
+}

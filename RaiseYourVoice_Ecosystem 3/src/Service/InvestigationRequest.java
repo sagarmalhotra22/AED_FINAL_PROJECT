f9aa8e0601ce70700f71323fb60e4f3d4b6b87ad
ShreyangiPrasad25 <prasad.shrey@northeastern.edu>
2022-04-24 12:29:23 -0400

    Investigation request created

---
+++ RaiseYourVoice_Ecosystem 3/src/Service/InvestigationRequest.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Service;
+
+/**
+ *
+ *  
+ */
+public class InvestigationRequest {
+    String Request_Id;
+    String Victim_email;
+    String Culprit_Name;
+    String Crime_Description;
+    String Crime_Location;
+    String Crime_Date; 
+    String Culprit_Image;
+    String Department;
+    String Assigned_To;
+    String Status;
+    String Notes;
+    String Request_Date;
+    String Update_Date;
+    String Test_Assigne;
+
+    
+    
+    
+}

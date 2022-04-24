4c6fe20981b2ee379fe90f23bce83c75a34338e5
ShreyangiPrasad25 <prasad.shrey@northeastern.edu>
2022-04-24 10:54:52 -0400

    Created victim class

---
+++ RaiseYourVoice_Ecosystem 3/src/Service/Victim.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Service;
+
+/**
+ *
+ *  
+ */
+public class Victim {
+    
+    String name;
+    long contact;
+    String email;
+    String password;
+    String city;
+    String security_question;
+    
+    
+
+    
+    
+    
+}

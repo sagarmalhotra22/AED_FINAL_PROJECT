a87b7cc395dd59cd88e0daa02346d47d473329d4
unknown <malhotra.sag@northeastern.edu>
2022-04-24 17:22:35 -0400

    created medical request fro victim requests

--- RaiseYourVoice_Ecosystem 3/src/Service/VictimRequests.java
+++ RaiseYourVoice_Ecosystem 3/src/Service/VictimRequests.java
@@ -62,5 +62,46 @@ public class VictimRequests {
         }
         return null;
     }
-
+         
+           public ArrayList<MedicalRequest> getVictimMedicalRequest() {
+        DBConnection DBcon = new DBConnection();
+        System.out.println("-------------Fetching Medical Request-------------");
+        Connection con= DBcon.getDbcon();
+        
+        
+        try{
+            PreparedStatement s = con.prepareStatement("Select * from MEDICAL_REQUEST");
+            System.out.println("-------------Fetching Medical requests for victim "+ "------------");
+            ResultSet r = s.executeQuery();
+            System.out.println("-------------Query Executed------------");
+            if(r==null)
+            {
+                JOptionPane.showMessageDialog( null, "Some error occured. Data fetch unsucccessfull. Please try after sometime.");
+            }
+            
+            while(r.next())
+            {
+                MedicalRequest medicalRequest = new MedicalRequest();
+                medicalRequest.setVictim_email((!r.getString("Victim_email").isBlank() || r.getString("Victim_email").isEmpty()) ? 
+                        r.getString("Victim_email") : null);
+                medicalRequest.setProblem_Description((r.getString("Problem_Description").isBlank() || r.getString("Problem_Description").isEmpty()) ? 
+                        r.getString("Problem_Description") : null);
+                medicalRequest.setAssigned_To((r.getString("ASSIGNED_TO").isBlank() || r.getString("ASSIGNED_TO").isEmpty()) ? 
+                        r.getString("ASSIGNED_TO") : null);
+                medicalRequest.setDepartment((r.getString("DEPARTMENT").isBlank() || r.getString("DEPARTMENT").isEmpty()) ? 
+                        r.getString("DEPARTMENT") : null);
+                medicalRequest.setStatus((r.getString("Status").isBlank() || r.getString("Status").isEmpty()) ?
+                        r.getString("Status") : null);
+                
+                if(!medicalRequest.getVictim_email().isEmpty() || !medicalRequest.getVictim_email().isBlank())
+                    medicalRequests.add(medicalRequest);
+            }
+            return medicalRequests;
+        }
+        catch (SQLException e1)
+        {
+            JOptionPane.showMessageDialog( null, "Some error occured. Data fetch unsucccessfull. Please try after sometime.");
+        }
+        return null;
+    }
 }

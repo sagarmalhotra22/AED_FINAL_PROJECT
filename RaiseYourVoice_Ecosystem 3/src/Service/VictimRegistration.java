6c5b0e8e6b1cdeed53dbb2b1420957884c771b61
ShreyangiPrasad25 <prasad.shrey@northeastern.edu>
2022-04-24 12:00:30 -0400

    Added database connectivity for registration

--- RaiseYourVoice_Ecosystem 3/src/Service/VictimRegistration.java
+++ RaiseYourVoice_Ecosystem 3/src/Service/VictimRegistration.java
@@ -19,6 +19,29 @@ public class VictimRegistration {
     public VictimRegistration(Victim victim) {
         this.victim = victim;
         
-      
+         DBConnection DBcon = new DBConnection();
+        System.out.println("-------------Registering Victim------------------");
+	Connection con= DBcon.getDbcon();
+	try{
+	
+	PreparedStatement stmt=con.prepareStatement("insert into VICTIM values(?,?,?,?,?,?)");
+    
+	                stmt.setString(1,victim.getName());
+                        stmt.setLong(2, victim.getContact());
+                        stmt.setString(3,victim.getEmail());
+                        stmt.setString(4, victim.getPassword());
+                        stmt.setString(5,victim.getCity());
+                        stmt.setString(6,victim.getSecurity_question());
+                        stmt.executeQuery();
+        JOptionPane.showMessageDialog( null, victim.getName()+ " Registered Sucessfully");
+	System.out.println("-------------Registeration Successful------------------");
+        stmt.close();
+	con.close();
+               
+
+            } catch (SQLException e1) {
+                JOptionPane.showMessageDialog( null, "Some Error Occured. Registration Unsucccessfull. Please Try After Sometime.");
+            }
+    }
     
 }

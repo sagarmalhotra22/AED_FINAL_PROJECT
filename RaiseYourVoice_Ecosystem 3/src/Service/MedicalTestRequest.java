a9ae2380aba8d924cbfe58345a0a051214156794
unknown <malhotra.sag@northeastern.edu>
2022-04-25 21:53:59 -0400

    added getter and setter for test request

--- RaiseYourVoice_Ecosystem 3/src/Service/MedicalTestRequest.java
+++ RaiseYourVoice_Ecosystem 3/src/Service/MedicalTestRequest.java
@@ -17,7 +17,61 @@ public class MedicalTestRequest {
     public String Assigned_To;
     public String Status;
     public String Notes;
+public String getRequest_Id() {
+        return Request_Id;
+    }
 
+    public void setRequest_Id(String Request_Id) {
+        this.Request_Id = Request_Id;
+    }
+
+    public String getTest_Id() {
+        return Test_Id;
+    }
+
+    public void setTest_Id(String Test_Id) {
+        this.Test_Id = Test_Id;
+    }
+
+    public String getVictim_Email() {
+        return Victim_Email;
+    }
+
+    public void setVictim_Email(String Victim_Email) {
+        this.Victim_Email = Victim_Email;
+    }
+
+    public String getDepartment() {
+        return Department;
+    }
+
+    public void setDepartment(String Department) {
+        this.Department = Department;
+    }
+
+    public String getAssigned_To() {
+        return Assigned_To;
+    }
+
+    public void setAssigned_To(String Assigned_To) {
+        this.Assigned_To = Assigned_To;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String Status) {
+        this.Status = Status;
+    }
+
+    public String getNotes() {
+        return Notes;
+    }
+
+    public void setNotes(String Notes) {
+        this.Notes = Notes;
+    }
     
     
     

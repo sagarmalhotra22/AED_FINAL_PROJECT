bd04f27612f730763df81d157a5de65329be4e33
mahakbhatia <99061247+mahakbhatia@users.noreply.github.com>
2022-04-26 13:10:05 -0400

    Added admin data request

---
+++ RaiseYourVoice_Ecosystem 3/src/Service/AdminDataRequest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Service;
+
+/**
+ *
+ *  
+ */
+public class AdminDataRequest {
+    
+    public String requestId;
+    public String victimEmail;
+    public String Department;
+    public String Assigned_To;
+   
+ 
+    
+}

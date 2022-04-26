fda0aed244178fd3ddbcc02555beffee4c93fb17
unknown <malhotra.sag@northeastern.edu>
2022-04-25 21:53:30 -0400

    added medical test request

---
+++ RaiseYourVoice_Ecosystem 3/src/Service/MedicalTestRequest.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Service;
+
+/**
+ *
+ *  
+ */
+public class MedicalTestRequest {
+    
+    public String Request_Id;
+    public String Test_Id;
+    public String Victim_Email;
+    public String Department;
+    public String Assigned_To;
+    public String Status;
+    public String Notes;
+
+    
+    
+    
+    
+}

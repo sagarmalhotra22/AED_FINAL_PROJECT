d2a6b263c5a4403a169bc8825c6c5c4405d5b404
unknown <malhotra.sag@northeastern.edu>
2022-04-25 21:27:56 -0400

    added victimand staff methods for password recovery

--- RaiseYourVoice_Ecosystem 3/src/Service/RecoverPassword.java
+++ RaiseYourVoice_Ecosystem 3/src/Service/RecoverPassword.java
@@ -30,7 +30,81 @@ public class RecoverPassword {
      DBConnection DBcon = new DBConnection();
         
         Connection con= DBcon.getDbcon();
-    
+    if(user.equalsIgnoreCase("Victim")){
+                try{
+                    PreparedStatement stmt=con.prepareStatement("Select password,security_question from VICTIM where EMAIL=?");
+
+                               
+                                    stmt.setString(1,email);
+                                   System.out.println("-------------Recovering Password-------------");
+                                    ResultSet rset=stmt.executeQuery();
+
+                    System.out.println("query executed");
+                    while(rset.next()){
+                             pass =rset.getString("password");
+                             question = rset.getString("security_question");
+                    }
+                  
+                    if(pass.isEmpty()){
+                     JOptionPane.showMessageDialog(null, "Email not present in dataset");
+
+                    }
+                    else if(!(question.trim().equalsIgnoreCase(petname.trim()))){
+                        JOptionPane.showMessageDialog(null, "Incorrect Pet name");
+                    }
+                    else
+                    {JOptionPane.showMessageDialog(null, "Password Recovery Successfull");
+                    rpass = pass;
+                    }
+
+                    stmt.close();
+                    con.close();
+
+
+                } 
+                catch (SQLException e1) {
+                        JOptionPane.showMessageDialog( null, "Some Error Occured.Password Recovery Unsucccessfull. Please Try After Sometime.");
+                    }
+             
+     
+       }
+        else if (user.equalsIgnoreCase("Staff")){
+         try{
+                    PreparedStatement stmt=con.prepareStatement("Select password from STAFF where EMAIL=?");
+
+                                    
+                                    stmt.setString(1,email);
+                                    ResultSet rset=stmt.executeQuery();
+
+                    System.out.println("query executed");
+                    while(rset.next()){
+                             pass =rset.getString("password");
+                              question = rset.getString("security_question");
+                             
+                    }
+                    if(pass.isEmpty()){
+                     JOptionPane.showMessageDialog(null, "Email not present in dataset");
+
+                    }
+                    else if(!(question.trim().equalsIgnoreCase(petname.trim()))){
+                        JOptionPane.showMessageDialog(null, "Incorrect Pet name");
+                    }
+                    else
+                    {JOptionPane.showMessageDialog(null, "Password Recovery Successfull");
+                    rpass = pass; 
+                    }
+
+                    
+                    con.close();
+
+
+                } 
+                catch (SQLException e1) {
+                        JOptionPane.showMessageDialog( null, "Some Error Occured.Password Recovery Unsucccessfull. Please Try After Sometime.");
+                    }
+             
+     
+        }
         
      return pass; }
 }

b421245e7d021d985d3ed5a3f1622b3447deae7d
mahakbhatia <99061247+mahakbhatia@users.noreply.github.com>
2022-04-28 16:27:08 -0400

    Created law service for Justice

---
+++ RaiseYourVoice_Ecosystem 3/src/Service/LawService.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Service;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ *  
+ */
+public class LawService {
+    
+    public ArrayList<InvestigationRequest> getLawyerRequests(String email)
+    {
+        ArrayList<InvestigationRequest> lawRequests = new ArrayList<InvestigationRequest>();
+        DBConnection DBcon = new DBConnection();
+        System.out.println("-------------Fetching Investigation Request-------------");
+        Connection con= DBcon.getDbcon();
+        
+        String query = prepareQuery(String.valueOf(Constants.LawOperations.GET_ALLOCATED_REQUESTS), "", "");
+        
+        if(query.isBlank() || query.isEmpty())
+            return null;
+        
+        try{
+            PreparedStatement s = con.prepareCall(query);
+            s.setString(1, String.valueOf(Constants.Status.ASSIGNED_TO_LAWYER));
+            System.out.println("-------------Fetching law requests from " + Constants.investigationTableName + "------------");
+            ResultSet r = s.executeQuery();
+            System.out.println("-------------Query Executed------------");
+            if(r==null)
+            {
+                JOptionPane.showMessageDialog( null, "Some error occured. Data fetch unsucccessfull. Please try after sometime.");
+            }
+            
+            while(r.next())
+            {
+                InvestigationRequest investigationRequest = new InvestigationRequest();
+                investigationRequest.setRequest_Id((!r.getString("Request_Id").isBlank() || r.getString("Request_Id").isEmpty()) ? 
+                        r.getString("Request_Id") : null);
+                investigationRequest.setVictim_email((!r.getString("Victim_email").isBlank() || r.getString("Victim_email").isEmpty()) ? 
+                        r.getString("Victim_email") : null);
+                investigationRequest.setCrime_Description((r.getString("Crime_Description").isBlank() || r.getString("Crime_Description").isEmpty()) ? 
+                        r.getString("Crime_Description") : null);
+                investigationRequest.setStatus((r.getString("Status").isBlank() || r.getString("Status").isEmpty()) ?
+                        r.getString("Status") : null);
+                
+                if(!investigationRequest.getVictim_email().isEmpty() || !investigationRequest.getVictim_email().isBlank())
+                    lawRequests.add(investigationRequest);
+            }
+            s.close();
+            con.close();
+            return lawRequests;
+        }
+        catch(SQLException e)
+        {
+            JOptionPane.showMessageDialog( null, "Some error occured. Data fetch unsucccessfull. Please try after sometime.");
+        }
+        return null;
+    }
+    
+   
+    
+    private String prepareQuery(String operation, String role, String email)
+    {
+        if(operation.equals(String.valueOf(Constants.LawOperations.GET_ALLOCATED_REQUESTS)))
+        {
+            return "SELECT * FROM " + Constants.investigationTableName + " WHERE Status=?";
+        }
+        if(operation.equals(String.valueOf(Constants.LawOperations.PRESENT_CASE)))
+        {
+            return "UPDATE " + Constants.investigationTableName + " SET Status=?, Assigned_To=?, Notes=? WHERE Request_Id=?";
+        }
+        if(operation.equals(String.valueOf(Constants.LawOperations.GET_JUDGE_REQUESTS)))
+        {
+            return "SELECT * FROM " + Constants.investigationTableName + " WHERE Status=?";
+        }
+        if(operation.equals(String.valueOf(Constants.LawOperations.PASS_VERDICT)))
+        {
+            return "UPDATE " + Constants.investigationTableName + " SET Status=?, Assigned_To=?, Notes=? WHERE Request_Id=?";
+        }
+        return null;
+    }
+    
+}

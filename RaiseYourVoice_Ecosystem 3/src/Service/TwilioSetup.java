6da5b75ad28031a6e686e33ac06d7c44a614b902
mahakbhatia <99061247+mahakbhatia@users.noreply.github.com>
2022-04-30 14:20:14 -0400

    added twilio setup

--- RaiseYourVoice_Ecosystem 3/src/Service/TwilioSetup.java
+++ RaiseYourVoice_Ecosystem 3/src/Service/TwilioSetup.java
@@ -5,10 +5,17 @@
  */
 package Service;
 
-/**
- *
- *  
- */
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 public class TwilioSetup {
-    
-}
+    public static final String ACCOUNT_SID = System.getenv("AC48048d1137ccb2eca2897182686f768c");
+    public static final String AUTH_TOKEN = System.getenv("87e182acc4f08c4a2cd0b13de3ee9ec9");
+    String no;
+    public void sendMsg(String no) {
+        this.no =no;
+        String phn = "+1"+no;
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+    Message message = Message.creator(new PhoneNumber(phn),new PhoneNumber(""),"You have been successfully registered").create();
+    System.out.println(message.getSid());
+    }}

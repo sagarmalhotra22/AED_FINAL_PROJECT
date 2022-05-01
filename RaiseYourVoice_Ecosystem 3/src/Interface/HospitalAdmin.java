328ae55c15d186f65add0b3f74495eaf5efbc140
unknown <malhotra.sag@northeastern.edu>
2022-05-01 10:36:51 -0400

    Updated hosptal admin

--- RaiseYourVoice_Ecosystem 3/src/Interface/HospitalAdmin.java
+++ RaiseYourVoice_Ecosystem 3/src/Interface/HospitalAdmin.java
@@ -48,6 +48,7 @@ public class HospitalAdmin extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        lblCloseAdmin = new javax.swing.JLabel();
         lblCreateTitle = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
@@ -61,12 +62,26 @@ public class HospitalAdmin extends javax.swing.JFrame {
         TFReqId = new javax.swing.JTextField();
         btnUnalllocated = new javax.swing.JButton();
         btnAccept = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        lblAdminLogout = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
         setMinimumSize(new java.awt.Dimension(1550, 820));
+        setUndecorated(true);
         setPreferredSize(new java.awt.Dimension(956, 570));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        lblCloseAdmin.setBackground(new java.awt.Color(68, 168, 217));
+        lblCloseAdmin.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        lblCloseAdmin.setForeground(new java.awt.Color(255, 255, 255));
+        lblCloseAdmin.setText("X");
+        lblCloseAdmin.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lblCloseAdminMouseClicked(evt);
+            }
+        });
+        getContentPane().add(lblCloseAdmin, new org.netbeans.lib.awtextra.AbsoluteConstraints(1510, 0, 20, 30));
 
         lblCreateTitle.setBackground(new java.awt.Color(68, 168, 217));
         lblCreateTitle.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
@@ -105,20 +120,25 @@ public class HospitalAdmin extends javax.swing.JFrame {
                 {null, null, null, null, null, null}
             },
             new String [] {
-                "Patient Name", "Title 2", "Title 3", "Title 4", "null", "null"
+                "Request_Id", "Victim_Email", "Case_Desc", "Assigned_To", "Status", "Notes"
             }
         ) {
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, false, false
+                false, false, false, false, false, true
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 return canEdit [columnIndex];
             }
         });
+        tblDetails.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tblDetailsMouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(tblDetails);
 
-        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 170, 820, 300));
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 170, 820, 300));
 
         btnReject.setBackground(new java.awt.Color(255, 153, 51));
         btnReject.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
@@ -128,7 +148,7 @@ public class HospitalAdmin extends javax.swing.JFrame {
                 btnRejectActionPerformed(evt);
             }
         });
-        jPanel1.add(btnReject, new org.netbeans.lib.awtextra.AbsoluteConstraints(630, 700, 190, 40));
+        jPanel1.add(btnReject, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 700, 190, 40));
 
         btnTestDept.setBackground(new java.awt.Color(255, 153, 51));
         btnTestDept.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
@@ -138,7 +158,7 @@ public class HospitalAdmin extends javax.swing.JFrame {
                 btnTestDeptActionPerformed(evt);
             }
         });
-        jPanel1.add(btnTestDept, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 90, 230, 50));
+        jPanel1.add(btnTestDept, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 90, 230, 50));
 
         btnCounDept.setBackground(new java.awt.Color(255, 153, 51));
         btnCounDept.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
@@ -148,23 +168,24 @@ public class HospitalAdmin extends javax.swing.JFrame {
                 btnCounDeptActionPerformed(evt);
             }
         });
-        jPanel1.add(btnCounDept, new org.netbeans.lib.awtextra.AbsoluteConstraints(870, 90, 230, 50));
+        jPanel1.add(btnCounDept, new org.netbeans.lib.awtextra.AbsoluteConstraints(630, 90, 230, 50));
 
         TFAssDept.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        jPanel1.add(TFAssDept, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 630, 310, 40));
+        jPanel1.add(TFAssDept, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 620, 310, 40));
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         jLabel1.setText("Assigning Department:");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 620, 180, 50));
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 610, 180, 50));
 
         jLabel2.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         jLabel2.setText("Request Id:");
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 540, 180, 50));
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 530, 180, 50));
 
+        TFReqId.setEditable(false);
         TFReqId.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        jPanel1.add(TFReqId, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 550, 310, 40));
+        jPanel1.add(TFReqId, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 540, 310, 40));
 
         btnUnalllocated.setBackground(new java.awt.Color(255, 153, 51));
         btnUnalllocated.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
@@ -174,7 +195,7 @@ public class HospitalAdmin extends javax.swing.JFrame {
                 btnUnalllocatedActionPerformed(evt);
             }
         });
-        jPanel1.add(btnUnalllocated, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 90, 230, 50));
+        jPanel1.add(btnUnalllocated, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 90, 230, 50));
 
         btnAccept.setBackground(new java.awt.Color(255, 153, 51));
         btnAccept.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
@@ -184,7 +205,21 @@ public class HospitalAdmin extends javax.swing.JFrame {
                 btnAcceptActionPerformed(evt);
             }
         });
-        jPanel1.add(btnAccept, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 700, 190, 40));
+        jPanel1.add(btnAccept, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 700, 190, 40));
+
+        jLabel3.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Interface/Images/hospital4.png"))); // NOI18N
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(980, 90, 520, -1));
+
+        lblAdminLogout.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        lblAdminLogout.setForeground(new java.awt.Color(0, 51, 102));
+        lblAdminLogout.setText("Logout");
+        lblAdminLogout.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lblAdminLogoutMouseClicked(evt);
+            }
+        });
+        jPanel1.add(lblAdminLogout, new org.netbeans.lib.awtextra.AbsoluteConstraints(1470, 60, 70, 20));
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1550, 810));
 
@@ -211,6 +246,11 @@ public class HospitalAdmin extends javax.swing.JFrame {
             Boolean reponse = hospitalService.assignRequestToCounsellingDepartment(requestId);
             if(reponse != null)
                 JOptionPane.showMessageDialog(this, "Successfully Assigned to Counselling Department !");
+            populateTable(unallocatedTableName);
+            TFReqId.setEditable(true);
+            TFReqId.setText("");
+            TFReqId.setEditable(false);
+            TFAssDept.setText("");
             return;
         }
         if(department.equalsIgnoreCase("TEST DEPARTMENT"))
@@ -218,6 +258,11 @@ public class HospitalAdmin extends javax.swing.JFrame {
             Boolean reponse = hospitalService.assignRequestToTestDepartment(requestId);
             if(reponse != null)
                 JOptionPane.showMessageDialog(this, "Successfully Assigned to Testing Department !");
+            TFReqId.setEditable(true);
+            TFReqId.setText("");
+            TFReqId.setEditable(false);
+            TFAssDept.setText("");
+            populateTable(unallocatedTableName);
             return;
         }
         JOptionPane.showMessageDialog(this, "Invalid Department !");
@@ -226,14 +271,32 @@ public class HospitalAdmin extends javax.swing.JFrame {
 
     private void btnUnalllocatedActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnUnalllocatedActionPerformed
         populateTable(unallocatedTableName);
+                jLabel2.setVisible(true);
+        TFReqId.setVisible(true);
+        jLabel1.setVisible(true);
+        TFAssDept.setVisible(true);
+        btnAccept.setVisible(true);
+        btnReject.setVisible(true);
     }//GEN-LAST:event_btnUnalllocatedActionPerformed
 
     private void btnTestDeptActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTestDeptActionPerformed
         populateTable(testsTableName);
+        jLabel2.setVisible(false);
+        TFReqId.setVisible(false);
+        jLabel1.setVisible(false);
+        TFAssDept.setVisible(false);
+        btnAccept.setVisible(false);
+        btnReject.setVisible(false);
     }//GEN-LAST:event_btnTestDeptActionPerformed
 
     private void btnCounDeptActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCounDeptActionPerformed
         populateTable(counsellingTableName);
+                jLabel2.setVisible(false);
+        TFReqId.setVisible(false);
+        jLabel1.setVisible(false);
+        TFAssDept.setVisible(false);
+        btnAccept.setVisible(false);
+        btnReject.setVisible(false);
     }//GEN-LAST:event_btnCounDeptActionPerformed
 
     private void btnRejectActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRejectActionPerformed
@@ -261,9 +324,36 @@ public class HospitalAdmin extends javax.swing.JFrame {
             Boolean r = hospitalService.rejectCase(requestId, c);
             if(r)
                 JOptionPane.showMessageDialog(null, "Successfully rejected case");
+            populateTable(unallocatedTableName);
         }
         return;
     }//GEN-LAST:event_btnRejectActionPerformed
+
+    private void tblDetailsMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblDetailsMouseClicked
+        DefaultTableModel model = (DefaultTableModel) tblDetails.getModel();
+        int selectedRowIndex = tblDetails.getSelectedRow();
+        
+        if(selectedRowIndex < 0 ){
+            JOptionPane.showMessageDialog(this, "Please select a row to view");
+            return;
+        }
+        String requestId = String.valueOf(model.getValueAt(selectedRowIndex, 0));
+        TFReqId.setText(requestId);
+        
+    }//GEN-LAST:event_tblDetailsMouseClicked
+
+    private void lblCloseAdminMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblCloseAdminMouseClicked
+        // TODO add your handling code here:
+        this.dispose();
+        new HomePage().setVisible(true);
+    }//GEN-LAST:event_lblCloseAdminMouseClicked
+
+    private void lblAdminLogoutMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblAdminLogoutMouseClicked
+        // TODO add your handling code here:
+        JOptionPane.showMessageDialog(null,"Successfully Logout");
+        this.dispose();
+        new HomePage().setVisible(true);
+    }//GEN-LAST:event_lblAdminLogoutMouseClicked
 
     /**
      * @param args the command line arguments
@@ -310,8 +400,11 @@ public class HospitalAdmin extends javax.swing.JFrame {
     private javax.swing.JButton btnUnalllocated;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lblAdminLogout;
+    private javax.swing.JLabel lblCloseAdmin;
     private javax.swing.JLabel lblCreateTitle;
     private javax.swing.JTable tblDetails;
     // End of variables declaration//GEN-END:variables
@@ -347,9 +440,11 @@ public class HospitalAdmin extends javax.swing.JFrame {
             row[0] = r.getRequest_Id();
             row[1] = r.getVictim_email();
             row[2] = r.getProblem_Description();
+            row[3]=r.getAssigned_To();
+            row[4]=r.getStatus();
             //row[3] = r.getCaseDescription();
             //row[4] = r.getContact();
-            //row[5] = r.getCaseDescription();
+            row[5] = r.getNotes();
             model.addRow(row);
         }
     }
